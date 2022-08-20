54f1c1ff3a52109cc1e3964ab98d2b8fb13df737
testdev1 <testdev1@devma.il>
2022-08-20 20:44:41 +0300

    inito

---
+++ src/main/java/com/github/newbiemr/poroce/poroce1/Poroce1Application.java
@@ -0,0 +1,13 @@
+package com.github.newbiemr.poroce.poroce1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poroce1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poroce1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/newbiemr/poroce/poroce1/Poroce1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.newbiemr.poroce.poroce1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poroce1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
